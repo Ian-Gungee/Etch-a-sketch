d83784ffd1c0c9c5effdb809c614279f474bb739
Ethan N. Gunter <thethingthatskates@gmail.com>
2017-07-11 14:49:46 -0400

    Adds crucial pixel positioning to create a grid. Needs repositioning in page.

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-var x = 5;
-var y = 5;
+var pixelSize = 10;
+var x;
+var y;
 var pX = 0;
-var pY= 0;
+var pY = 0;
+var inputX = document.getElementById('width');
+var inputY = document.getElementById('height');
+var rad = 1;
 
 $(document).ready(function() {
     /* Once page has loaded */
     console.log('Page Loaded')
 
-    /* Sets text inputs to default x & y values 
-    document.getElementById('width').nodeValue = x;
-    document.getElementById('height').nodeValue = y;
-    */
+    /* Sets text inputs to default x & y values */
+    x = 10;
+    y = 10;
 
+    inputX.value = x;
+    inputY.value = y;
 
     drawCanvas();
+
 
     /* Activates and highlights pixels when moused over*/
     $('.pixel').mouseenter(function(){
@@ -25,41 +31,49 @@ $(document).ready(function() {
         $(this).css('background-color', 'black');
     });
 
-
-
 });
+
+
+/* Refreshes screen and draws a new canvas */
+function submit(){
+        x = inputX.value;
+        y = inputY.value;
+    var prompt = confirm("Rendering canvas: " + x + "x" + y + ". This will erase your current work. Ok?");
+    if (prompt == true) {
+        pX = 0;
+        pY = 0;
+        console.log("x: "+x);
+        console.log("y: "+y);
+        $('.canvas > .pixel').remove();
+        drawCanvas();
+    }
+
+}
+
 
 var newDiv;
 var canvas = document.getElementsByClassName('canvas');
 var pixels = document.getElementsByClassName('pixel');
 
+/* Nested for loops that draw pixels one by one, starting from top to bottom, then left to right */
 function drawCanvas() {
-    for (i=1; i <= x; i++) {
-        for (i=1; i <= y; i++) {
+    for (q=0; q < x; q++) {
+
+        for (w=0; w < y; w++) {
             newDiv = document.createElement('div');
             newDiv.className = 'pixel';
             newDiv.id = 'p' + pX + pY;
+            newDiv.style.left = ((pX*pixelSize).toString())+"px";
+            newDiv.style.top = ((pY*pixelSize).toString())+"px";
+            console.log("drawing: "+newDiv.id);
             $('.canvas').append(newDiv);
             pY += 1;
-            console.log('y = ' + pY);
         }
-
         newDiv.id = 'p' + pX + pY;
         $('.canvas').append(newDiv);
-        pY += 1;
-        console.log('y = ' + pY);
+        console.log("drawing: "+newDiv.id);
+        pX += 1;
+        pY = 0;
     }
     console.log('all pixels rendered');
-}
-
-/* Refreshes screen and draws a new canvas */
-function submit(){
-    var prompt = confirm("Rendering canvas: " + x + "x" + y + ". This will erase your current work. Ok?");
-    if (prompt == true) {
-        pX = 0;
-        pY = 0;
-        $('.canvas > .pixel').remove();
-        drawCanvas();
-    }
-
 }
